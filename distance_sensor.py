e2b0879e8047864f021921e3452c96947a3c1f7d
silaksila <silaksila27@gmail.com>
2021-02-18 17:13:57 +0100

    Hi from desktop

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,9 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
+
 def setup():
     GPIO.setmode(GPIO.BCM)
 
     GPIO.setup(18, GPIO.OUT)
-    GPIO.setup(17, GPIO.IN) #echo pin
+    GPIO.setup(17, GPIO.IN)  # echo pin
 
+
+def measure_distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+
+
+def distance_is_lower(leng):
+    if measure_distance() < leng:
+        print("Close!!!")
+        return True
+    return False
--- main_movement.py
+++ main_movement.py
@@ -1,80 +1,102 @@
 import RPi.GPIO as GPIO
-import keyboard 
+import keyboard
 import time
 import curses
+import distance_sensor
 
-#imput setup
+# imput setup
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
-#GPIO numbering
+######
+distance_sensor.setup()
+# GPIO numbering
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(14, GPIO.OUT)#Forward
-GPIO.setup(15, GPIO.OUT)# back
-GPIO.setup(23, GPIO.OUT)#Forward
-GPIO.setup(24, GPIO.OUT)# back
+GPIO.setup(14, GPIO.OUT)  # Forward
+GPIO.setup(15, GPIO.OUT)  # back
+GPIO.setup(23, GPIO.OUT)  # Forward
+GPIO.setup(24, GPIO.OUT)  # back
 
 GPIO.setup(21, GPIO.OUT)
 
-#servo
-volant= GPIO.PWM(21, 50)
+# servo
+volant = GPIO.PWM(21, 50)
 volant.start(0)
 volant.ChangeDutyCycle(6)
 time.sleep(1)
+
+running = False
 try:
-    #main script
+    # main script
     while True:
-        char= screen.getch()
-        if char== ord('q'): #quit
+        too_close = distance_sensor.distance_is_lower(5.0)
+
+        char = screen.getch()
+        if char == ord('q'):  # quit
             break
-        if char== ord('w'):#forward
-            #stop for a small amount of time
-            GPIO.output(14, False)
-            GPIO.output(15, False)
-            GPIO.output(23, False)
-            GPIO.output(24, False)
-            time.sleep(0.5)
-            
-            GPIO.output(14, False)
-            GPIO.output(15, True)
-            GPIO.output(23, False)
-            GPIO.output(24, True)
-        elif char== ord('s'): #bacward
-            #stop for a small ammount of time
-            GPIO.output(14, False)
-            GPIO.output(15, False)
-            GPIO.output(23, False)
-            GPIO.output(24, False)
-            time.sleep(0.5) 
-            GPIO.output(14, True)
-            GPIO.output(15, False)
-            GPIO.output(23, True)
-            GPIO.output(24, False)
-        elif char== ord('b'): #stop
-            GPIO.output(14, False)
-            GPIO.output(15, False)
-            GPIO.output(23, False)
-            GPIO.output(24, False)
+        if char == ord('w'):  # forward
+            # stop for a small amount of time
+            if running:
+                GPIO.output(14, False)
+                GPIO.output(15, False)
+                GPIO.output(23, False)
+                GPIO.output(24, False)
+                time.sleep(0.2)
 
+                GPIO.output(14, False)
+                GPIO.output(15, True)
+                GPIO.output(23, False)
+                GPIO.output(24, True)
+            else:
+                GPIO.output(14, False)
+                GPIO.output(15, True)
+                GPIO.output(23, False)
+                GPIO.output(24, True)
+                running = True
 
+        elif char == ord('s'):  # bacward
+            # stop for a small ammount of time
+            if running:
+                GPIO.output(14, False)
+                GPIO.output(15, False)
+                GPIO.output(23, False)
+                GPIO.output(24, False)
+                time.sleep(0.2)
+                GPIO.output(14, True)
+                GPIO.output(15, False)
+                GPIO.output(23, True)
+                GPIO.output(24, False)
+            else:
+                GPIO.output(14, False)
+                GPIO.output(15, True)
+                GPIO.output(23, False)
+                GPIO.output(24, True)
+                running = True
 
-        if char == ord('a'): #turn left
+        elif char == ord('b') or too_close:  # stop
+            GPIO.output(14, False)
+            GPIO.output(15, False)
+            GPIO.output(23, False)
+            GPIO.output(24, False)
+            running = False
+
+        if char == ord('a'):  # turn left
             volant.ChangeDutyCycle(10)
             time.sleep(1)
             volant.ChangeDutyCycle(6)
-        elif char == ord('d'): #turn righ
+        elif char == ord('d'):  # turn righ
             volant.ChangeDutyCycle(2)
             time.sleep(1)
             volant.ChangeDutyCycle(6)
         else:
             volant.ChangeDutyCycle(6)
             time.sleep(0.5)
-except KeyboardInterrupt :
+except KeyboardInterrupt:
     pass
-#cleanup
+# cleanup
 GPIO.output(14, False)
 GPIO.output(15, False)
 GPIO.output(23, False)
@@ -89,4 +111,3 @@ curses.echo()
 curses.endwin()
 GPIO.cleanup()
 print("DONE")
-
