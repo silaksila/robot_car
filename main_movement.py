6ceb64394c85f8fc701da22617115226f0a3ef86
silaksila <silaksila27@gmail.com>
2021-02-18 19:11:49 +0100

    "Update"

--- main_movement.py
+++ main_movement.py
@@ -1,3 +1,4 @@
+from typing_extensions import runtime
 import RPi.GPIO as GPIO
 import keyboard
 import time
@@ -28,71 +29,95 @@ volant.start(0)
 volant.ChangeDutyCycle(6)
 time.sleep(1)
 
-running = False
+########################################################################################
+
+
+def forward():
+    global moving
+    if moving:
+        GPIO.output(14, False)
+        GPIO.output(15, False)
+        GPIO.output(23, False)
+        GPIO.output(24, False)
+
+        time.sleep(0.2)
+
+        GPIO.output(14, False)
+        GPIO.output(15, True)
+        GPIO.output(23, False)
+        GPIO.output(24, True)
+    else:
+        GPIO.output(14, False)
+        GPIO.output(15, True)
+        GPIO.output(23, False)
+        GPIO.output(24, True)
+        moving = True
+
+
+def backward():
+    global moving
+    if moving:
+        GPIO.output(14, False)
+        GPIO.output(15, False)
+        GPIO.output(23, False)
+        GPIO.output(24, False)
+        time.sleep(0.2)
+        GPIO.output(14, True)
+        GPIO.output(15, False)
+        GPIO.output(23, True)
+        GPIO.output(24, False)
+    else:
+        GPIO.output(14, True)
+        GPIO.output(15, False)
+        GPIO.output(23, True)
+        GPIO.output(24, False)
+        moving = True
+
+
+def stop():
+    global moving
+    GPIO.output(14, False)
+    GPIO.output(15, False)
+    GPIO.output(23, False)
+    GPIO.output(24, False)
+    time.sleep(0.2)
+    moving = False
+
+###########################################################################################
+
+
+moving = False
 try:
     # main script
     while True:
-        too_close = distance_sensor.distance_is_lower(5.0)
+        too_close = distance_sensor.distance_is_lower(10.0)
 
         char = screen.getch()
         if char == ord('q'):  # quit
             break
         if char == ord('w'):  # forward
-            # stop for a small amount of time
-            if running:
-                GPIO.output(14, False)
-                GPIO.output(15, False)
-                GPIO.output(23, False)
-                GPIO.output(24, False)
-                time.sleep(0.2)
-
-                GPIO.output(14, False)
-                GPIO.output(15, True)
-                GPIO.output(23, False)
-                GPIO.output(24, True)
-            else:
-                GPIO.output(14, False)
-                GPIO.output(15, True)
-                GPIO.output(23, False)
-                GPIO.output(24, True)
-                running = True
+            forward()
 
         elif char == ord('s'):  # bacward
-            # stop for a small ammount of time
-            if running:
-                GPIO.output(14, False)
-                GPIO.output(15, False)
-                GPIO.output(23, False)
-                GPIO.output(24, False)
-                time.sleep(0.2)
-                GPIO.output(14, True)
-                GPIO.output(15, False)
-                GPIO.output(23, True)
-                GPIO.output(24, False)
-            else:
-                GPIO.output(14, False)
-                GPIO.output(15, True)
-                GPIO.output(23, False)
-                GPIO.output(24, True)
-                running = True
+            backward()
 
-        elif char == ord('b') or too_close:  # stop
-            GPIO.output(14, False)
-            GPIO.output(15, False)
-            GPIO.output(23, False)
-            GPIO.output(24, False)
-            running = False
+        if char == ord('b') or too_close:  # stop
+            stop()
 
         if char == ord('a'):  # turn left
-            volant.ChangeDutyCycle(10)
-            time.sleep(1)
-            volant.ChangeDutyCycle(6)
+            while char != curses.KEY_UP:
+                volant.ChangeDutyCycle(10)
+                char = screen.getch()
+            time.sleep(0.1)
+            volant.ChangeDutyCycle(6.5)
         elif char == ord('d'):  # turn righ
-            volant.ChangeDutyCycle(2)
-            time.sleep(1)
-            volant.ChangeDutyCycle(6)
+            while char != curses.KEY_UP:
+                volant.ChangeDutyCycle(2)
+                char = screen.getch()
+            time.sleep(0.1)
+            volant.ChangeDutyCycle(6.5)
         else:
-            volant.ChangeDutyCycle(6)
+            volant.ChangeDutyCycle(6.5)
             time.sleep(0.5)
 except KeyboardInterrupt:
     pass
